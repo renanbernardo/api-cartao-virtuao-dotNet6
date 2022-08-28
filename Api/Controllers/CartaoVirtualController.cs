9b1ac0dd9b95709980e191ef7eb1a867c169f9da
Renan Bernardo <renan.brnrd@gmail.com>
2022-08-28 16:53:29 -0300

    Cria o Controller

---
+++ Api/Controllers/CartaoVirtualController.cs
@@ -0,0 +1,35 @@
+using Domain.Commands.Inputs;
+using Domain.Entities;
+using Domain.Handlers;
+using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Commands;
+
+namespace Api.Controllers;
+
+[Route("v1/cartoes")]
+public class CartaoVirtualController : Controller
+{
+    private readonly ICartaoVirtualRepository _repository;
+    
+    public CartaoVirtualController(IServiceProvider serviceProvider)
+    {
+        _repository = serviceProvider.GetService<ICartaoVirtualRepository>();
+    }
+
+    [HttpGet]
+    [Route("{email}")]
+    public IEnumerable<CartaoVirtual> Get(string email)
+    {
+        var cartoes = _repository.ListarCartaoVirtual(email);
+        return cartoes;
+    }
+
+    [HttpPost]
+    [Route("")]
+    public ICommandResult Post([FromBody] GerarCartaoVirtualCommand)
+    {
+        var handler = new GerarCartaoVirtualHandler(_repository);
+        return handler.Handle(command);
+    }
+}
---
+++ Shared/Commands/ICommandResult.cs
@@ -0,0 +1,8 @@
+namespace Shared.Commands;
+
+public interface ICommandResult
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+}
